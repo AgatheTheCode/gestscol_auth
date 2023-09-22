d747159029bc831e6c5b375edd63b8502ccd6ef0
Agathe <agathe.dufour.marin@gmail.com>
2023-09-22 09:06:01 +0200

    gitflow-hotfix-stash: install_livewire

--- app/Http/Controllers/Admin/ManagerController.php
+++ app/Http/Controllers/Admin/ManagerController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\AdminFormationController;
 use App\Http\Controllers\GroupController;
 
 use App\Models\Edt;
+use App\Models\Manager;
 use App\Models\Student;
 use App\Models\Formation;
 use App\Models\Group;
@@ -19,21 +20,25 @@ use App\Http\Requests\EdtRequest;
 use App\Http\Requests\StudentRequest;
 use App\Http\Requests\FormationRequest;
 use App\Http\Requests\GroupRequest;
+use JetBrains\PhpStorm\NoReturn;
 
 
 class ManagerController extends Controller
 {
+
     //ce controler doit permettre d'afficher tout les cours d'un journée, et au click sur un cours, afficher les étudiants de ce cours
-    public function index()
+    public function index(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-        $edts = Edt::all();
+        $this->authorize('viewAny', Manager::class);
+        $edt = Edt::all();
         return view('admin.manager.index', [
-            'edts' => $edts
+            'edt' => $edt
         ]);
     }
 
-    public function show(Edt $edt)
+    public function show(Edt $edt): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
+        $this->authorize('view', $edt);
         return view('admin.manager.show', [
             'edt' => $edt
         ]);
